907345177509d76d8a56261eb889d967563574e7
Gustavo Henrique Pinto <ghp2201@gmail.com>
2022-04-15 11:24:59 -0300

    Add dagster jobs and operations

---
+++ src/dagster/operations.py
@@ -0,0 +1,18 @@
+from src.interfaces.loader import DatabaseLoader
+from src.interfaces.parser import Parser
+from src.interfaces.webcrawler import Webcrawler
+
+from dagster import op
+
+
+@op
+def extract(extractor: Webcrawler) -> list[dict]:
+    return extractor.extract_data()
+
+@op
+def parse(parser: Parser, data: list[dict]) -> list[dict]:
+    return parser.parse_data(data)
+
+@op
+def load(loader: DatabaseLoader, data: list[dict]) -> None:
+    return loader.load('news', data)
